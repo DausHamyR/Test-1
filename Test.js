611c6fa836a74ad59ed532f5ff01f5843d71a9be
DausHamyR <120082873+DausHamyR@users.noreply.github.com>
2022-12-08 13:33:59 +0700

    bootstrap 5 Latihan

---
+++ Test.js
@@ -0,0 +1,272 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <link type="text/css" rel="stylesheet" href="https://eduwork.id/css/reset.css">
+    <link type="text/css" rel="stylesheet" href="https://eduwork.id/css/app.css">
+    <link type="text/css" rel="stylesheet" href="https://eduwork.id/css/plugins.css">
+    <link type="text/css" rel="stylesheet" href="https://eduwork.id/css/style.css">
+    <link type="text/css" rel="stylesheet" href="https://eduwork.id/css/landing-page.style.css">
+    <link type="text/css" rel="stylesheet" href="https://eduwork.id/css/landing-page-mobile.style.css">
+    <link type="text/css" rel="stylesheet" href="https://eduwork.id/css/single-class.css">
+    <link type="text/css" rel="stylesheet" href="https://eduwork.id/css/color.css">
+    <link type="text/css" rel="stylesheet" href="https://eduwork.id/css/jquery.datetimepicker.css">
+
+    <style>
+        .invalid-feedback { color: red ; }
+        .tooltiped {
+          position: relative;
+          z-index: 99999;
+    
+        }
+    
+        .tooltiped .tooltiptexted {
+          visibility: hidden;
+          width: 120px;
+          background-color: black;
+          color: #fff;
+          text-align: center;
+          padding: 5px 0;
+          border-radius: 6px;
+    
+          position: absolute;
+          z-index: 1;
+        }
+    
+        .tooltiped:hover .tooltiptexted {
+          visibility: visible;
+        }
+    </style>
+    <style type="text/css">
+        @media  only screen and (max-width: 1245px) {
+            .jumbotron{
+                width: 100%;
+            }
+            .jumbotron div{
+                display: d-none;
+            }
+            html{
+                overflow-x: auto;
+            }
+        }
+    </style>
+    <style>
+        .banner-text{
+            width: 100%;
+        }
+        .banner {
+            height: auto;
+            margin-bottom: 0px;
+        }
+        .banner-feature .banner-title{
+            line-height: normal;
+        }
+        .banner-subtitle{
+            font-weight: bold;
+            font-size: 24px;
+            line-height: 36px;
+            letter-spacing: 0.01em;
+            margin-bottom: 22px;
+            text-align: left;
+        }
+        .banner-feature{
+            padding-top: 0;
+        }
+        .banner-feature .banner-title{
+            padding-bottom: 24px;
+            max-width: unset;
+        }
+        .banner-feature .banner-sub-title{
+            margin-bottom: 48px;
+            font-size: 24px;
+            font-weight: bold;
+            color: #757575;
+        }
+        .banner-feature .feature-photo-icon.bg-transparent{
+            background-color: transparent!important;
+        }
+        .banner-basic{
+            padding-bottom: 1px;
+        }
+        .chips{
+            margin: 5px;
+        }
+        .banner-basic .title.text-24{
+            font-size: 24px!important;
+        }
+        .banner-basic .subtitle.text-18{
+            font-size: 18px;
+        }
+        .banner-feature .banner-content.py-22{
+            padding-bottom: 22px!important;
+            padding-top: 22px!important;
+        }
+        .banner-btn button, .banner-btn a{
+            font-size: 18px;
+        }
+        .pricing ul li{
+            color: black;
+            list-style: disc;
+        }
+    </style>
+    <style>
+        @media  only screen and (max-width: 1064px) {
+          .banner-feature .banner-sub-title{
+            font-weight: bold;
+              font-size: 18px;
+              letter-spacing: 0.01em;
+              margin-bottom: 22px;
+              max-width: 624px;
+              color: #757575;
+              margin-left: auto;
+              margin-right: auto;
+          }
+          .banner-feature .banner-content{
+            padding: 0 15px!important;
+          }
+        }
+    </style>
+    <style>
+        .jumbutton{
+          padding-top: 15px;
+          padding-bottom: 15px;
+        }
+        .jumbutton-active{
+          background-color: rgb(255, 255, 255);
+          box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+          border-top-left-radius: 10px;
+          border-top-right-radius: 10px;
+        }
+        .jumbutton:hover{
+          background-color: rgb(255, 255, 255);
+          box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+          border-top-left-radius: 10px;
+          border-top-right-radius: 10px;
+      
+        }
+      </style>
+
+  </head>
+  <body style="background-color: #F6FCFF!important;">
+    <div id="main" style="opacity: 1;
+    background-color: #F6FCFF;
+    ">
+    <header class="main-header fs-header sticky nav-colored" id="section-navigation">
+            <div class="header-inner container">
+                <div class="logo-holder">
+                    <a href="https://eduwork.id"><img class="photo" src="https://eduwork.id/templates/v1/landing/eduwork_light.png" /></a>
+                </div>
+                                <a class="add-list ask-on-register hidden-xs btn-auth-nav"
+                                    href="https://eduwork.id/m"
+                                       style="cursor: pointer;background-color: #FFD200!important;color: black;">
+                        Register <span><i class="fa fa-plus"></i></span>
+                    </a>
+                    <div class="show-reg-form modal-open native-register hidden-xs btn-auth-nav" id="link-login" onClick="$('.tab-login').click()">
+                        <span class="btn-auth-text color-white xs-color-dark-2" style="color: white;">Login</span>
+                    </div>
+
+
+    <div class="nav-button-wrap color-bg">
+        <div class="nav-button">
+            <span></span><span></span><span></span>
+        </div>
+    </div>
+    <div class="nav-holder main-menu">
+        <nav>
+            <ul>
+                <li> <a href="https://eduwork.id" class=" color-white xs-color-dark-2">Home</a> </li>
+                <li>
+                    <a href="https://eduwork.id/kelas" class=" color-white xs-color-dark-2">
+                        Kelas
+                        <i class="fas fa-caret-down"></i>
+                    </a>
+                <ul style="width: 300px">
+                     <li>
+                     <a href="https://eduwork.id/kelas?type=private">Private Mentoring </a>
+                     </li>
+                     <li>
+                     <a href="https://eduwork.id/kelas?type=webinar">Webinar </a>
+                     </li>
+                     <li>
+                     <a href="https://eduwork.id/kelas?type=mini_bootcamp">Mini Bootcamp </a>
+                     </li>
+                </ul>
+                </li>
+                <li class="hidden-lg hidden-md">
+                    <a href="#" class="modal-open" onClick="$('#link-login').click()">Sign in</a>
+                </li>
+                <li class="hidden-lg hidden-md">
+                    <a href="https://eduwork.id/m">Register</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    </div>
+    </header>
+    <!-- Test-->
+    <style>
+        .text-title-banner{
+          font-size: 40px;
+        }
+        .text-subtitle-banner {
+          font-size: 18px;
+        }
+        </style>
+        
+        <section class="content" style="margin-top: -80px;padding-bottom: 0;background-color: #F6FCFF;">
+            <div class="banner xs-pt-96" style="padding-top: 142px;" id="first-banner">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-5 xs-mb-48" style="margin-top: 50px">
+                            <div class="banner-text text-title-banner xs-text-36 xs-text-center">Pelatihan dan Penyaluran kerja!</div>
+                            <div class="banner-sub-text text-subtitle-banner xs-text-center" style="font-weight: 900;">
+                                <div>
+                                    Dapatkan gaji s.d lebih dari 10jt! Yuk, Pilih program sesuai dengan yang kamu inginkan!
+                                </div>
+                            </div>
+                            <div class="banner-btn xs-text-center">
+                                <a href="https://eduwork.id/m" class="mx-28 px-71 xs-w-full" id="register-homepage" style="position: relative;padding-right:36px!important;padding-left:36px!important;text-align: center;margin-top: 18px;display: inline-block;background-color: #FFD200;color: black;margin-right: 14px;">
+                                  Daftar
+                                 <span style="position: absolute;transform: translate(0%,-50%);border-radius: 99999px;margin-top: -8px;font-size: 12px;color: white;background-color: red;padding: 5px 11px">Free</span>
+                                </a>
+                                <a href="https://eduwork.id/m?class_id=148" id="konsul-homepage" class="mx-28 px-71 xs-w-full" style="padding-right:36px!important;padding-left:36px!important;text-align: center;background-color: black;margin-top: 18px;display: inline-block;">
+                                Konsul Dulu
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-sm-7">
+                            <img src="https://eduwork.id/templates/v1/landing/aniq.png" class="xs-ml--25" style="width: 100%;height: auto;" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+<!--Test-->
+<div class="mb-60 xs-mb-0" style="background-color: #E8F7FF;">
+    <div class="container py-37">
+        <div class="row d-flex xs-display-block" style="align-items: center;">
+            <div class="col-sm-6 d-flex align-items-center" style="height: 100%;">
+                <h1 class="text-36 font-800 xs-mb-24">Apa itu Eduwork?</h1>
+            </div>
+            <div class="col-sm-6">
+                <div class="text-18 font-500 text-left">
+                    Platform Pelatihan & Penyaluran kerja. Kamu bisa belajar dari 0 sampai bisa dan dapat kerja, dijamin!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
